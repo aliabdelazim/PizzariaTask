fbdeda4941bfd9b308f7bbf32cbf7f8356821658
a.abdelazim <a.abdelazim@theahoyapp.com>
2023-01-19 17:42:07 +0400

    first commit

--- dist/out-tsc/restaurant/dtos/orders.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.OrdersDTO = void 0;
-class OrdersDTO {
-}
-exports.OrdersDTO = OrdersDTO;
-//# sourceMappingURL=orders.dto.js.map
